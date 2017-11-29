dbc09341f9bbe4607854fbe2654a9b8fdb4c5790
Andrew Hercules <andrewhercules@gmail.com>
2017-11-29 23:05:00 +0000

    Refactored code by creating a play_fizzbuzz function

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,20 +1,21 @@
 def divisible_by_three(num):
-    if (is_divisible_by(num, 3)):
-        return 'fizz'
-    else:
-        return num
+    return is_divisible_by(num, 3)
 
 def divisible_by_five(num):
-    if (is_divisible_by(num, 5)):
-        return 'buzz'
-    else:
-        return num
+    return is_divisible_by(num, 5)
 
 def divisible_by_three_or_five(num):
-    if (is_divisible_by(num, 15)):
-        return 'fizzbuzz'
-    else:
-        return num
+    return is_divisible_by(num, 15)
 
 def is_divisible_by(number, divisor):
     return number % divisor == 0
+
+def play_fizzbuzz(number):
+    if (divisible_by_three_or_five(number)):
+        return 'fizzbuzz'
+    elif (divisible_by_three(number)):
+        return 'fizz'
+    elif (divisible_by_five(number)):
+        return 'buzz'
+    else:
+        return number
--- fizzbuzz/test/fizzbuzz_test.py
+++ fizzbuzz/test/fizzbuzz_test.py
@@ -5,17 +5,27 @@ sys.path.insert(0, myPath + '/../')
 import fizzbuzz
 
 def test_divisible_by_three():
-    assert (fizzbuzz.divisible_by_three(3) == 'fizz')
-    assert (fizzbuzz.divisible_by_three(4) == 4)
+    assert (fizzbuzz.divisible_by_three(3) == True)
+    assert (fizzbuzz.divisible_by_three(4) == False)
 
 def test_divisible_by_five():
-    assert (fizzbuzz.divisible_by_five(5) == 'buzz')
-    assert (fizzbuzz.divisible_by_five(8) == 8)
+    assert (fizzbuzz.divisible_by_five(5) == True)
+    assert (fizzbuzz.divisible_by_five(8) == False)
 
 def test_divisible_by_three_or_five():
-    assert (fizzbuzz.divisible_by_three_or_five(15) == 'fizzbuzz')
-    assert (fizzbuzz.divisible_by_three_or_five(21) == 21)
+    assert (fizzbuzz.divisible_by_three_or_five(15) == True)
+    assert (fizzbuzz.divisible_by_three_or_five(21) == False)
 
 def test_is_divisible_by():
     assert (fizzbuzz.is_divisible_by(6, 3) == True)
     assert (fizzbuzz.is_divisible_by(17, 9) == False)
+
+def test_play_fizzbuzz():
+    assert (fizzbuzz.play_fizzbuzz(3) == 'fizz')
+    assert (fizzbuzz.play_fizzbuzz(5) == 'buzz')
+    assert (fizzbuzz.play_fizzbuzz(15) == 'fizzbuzz')
+    assert (fizzbuzz.play_fizzbuzz(21) == 'fizz')
+    assert (fizzbuzz.play_fizzbuzz(35) == 'buzz')
+    assert (fizzbuzz.play_fizzbuzz(60) == 'fizzbuzz')
+    assert (fizzbuzz.play_fizzbuzz(11) == 11)
+    assert (fizzbuzz.play_fizzbuzz(34) == 34)
