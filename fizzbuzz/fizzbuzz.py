652f8be19b8155d8428569c0bd881567552baba0
Andrew Hercules <andrewhercules@gmail.com>
2017-11-29 22:32:30 +0000

    Updated functions to return numbers if not divisible by 3, 5, or 15

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,11 +1,17 @@
 def divisible_by_three(num):
     if (num % 3 == 0):
         return 'fizz'
+    else:
+        return num
 
 def divisible_by_five(num):
     if (num % 5 == 0):
         return 'buzz'
+    else:
+        return num
 
 def test_divisible_by_three_or_five(num):
     if (num % 15 == 0):
         return 'fizzbuzz'
+    else:
+        return num
--- fizzbuzz/test/fizzbuzz_test.py
+++ fizzbuzz/test/fizzbuzz_test.py
@@ -6,9 +6,12 @@ import fizzbuzz
 
 def test_divisible_by_three():
     assert (fizzbuzz.divisible_by_three(3) == 'fizz')
+    assert (fizzbuzz.divisible_by_three(4) == 4)
 
 def test_divisible_by_five():
     assert (fizzbuzz.divisible_by_five(5) == 'buzz')
+    assert (fizzbuzz.divisible_by_five(8) == 8)
 
 def test_divisible_by_three_or_five():
     assert (fizzbuzz.test_divisible_by_three_or_five(15) == 'fizzbuzz')
+    assert (fizzbuzz.test_divisible_by_three_or_five(21) == 21)
